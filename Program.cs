c705ecbc57ee14321d64554061cc6ee0ee8fadb9
Wanessabds <wanessa.nessap@gmail.com>
2023-06-02 19:48:54 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine(" *** Tabuada ***");
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.Write("Digite um número: ");
+
+Console.ForegroundColor = ConsoleColor.Black; 
+int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+int multiplo = 1;
+int resultado;
+
+
+while ( multiplo <= 10)
+{
+    resultado = numeroDigitado * multiplo;
+    Console.WriteLine($"{numeroDigitado} X {multiplo} = {resultado}");
+    multiplo++;
+}
